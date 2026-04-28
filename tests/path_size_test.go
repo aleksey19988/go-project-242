3a0b7aaad831411f2feaae5984b7a4e61c5fbd8b
Aleksey Kononenko <a.knnnk@mail.ru>
2026-04-28 16:04:49 +0500

    - Перенос строки unix вместо windows

--- tests/path_size_test.go
+++ tests/path_size_test.go
@@ -9,16 +9,16 @@ import (
 
 func TestPath(t *testing.T) {
 	res := functions.GetPathSize("../testdata", false, false)
-	require.Equal(t, "33760B", res)
+	require.Equal(t, "32248B", res)
 
 	res = functions.GetPathSize("../testdata", false, true)
-	require.Equal(t, "40682B", res)
+	require.Equal(t, "39170B", res)
 
 	res = functions.GetPathSize("../testdata", true, false)
-	require.Equal(t, "33.0KB", res)
+	require.Equal(t, "31.5KB", res)
 
 	res = functions.GetPathSize("../testdata", true, true)
-	require.Equal(t, "39.7KB", res)
+	require.Equal(t, "38.3KB", res)
 
 	res = functions.GetPathSize("otherpath", true, true)
 	require.Equal(t, "Error: open otherpath: no such file or directory", res)
@@ -29,11 +29,11 @@ func TestPath(t *testing.T) {
 
 func TestFile(t *testing.T) {
 	res := functions.GetPathSize("../testdata/text.txt", false, false)
-	require.Equal(t, "26838B", res)
+	require.Equal(t, "25326B", res)
 
 	res = functions.GetPathSize("../testdata/text.txt", false, true)
 	require.Equal(t, "0B", res)
 
 	res = functions.GetPathSize("../testdata/text.txt", true, false)
-	require.Equal(t, "26.2KB", res)
+	require.Equal(t, "24.7KB", res)
 }
