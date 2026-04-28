4fdfd385d34783addb8b9d4138ad274029357927
Aleksey Kononenko <a.knnnk@mail.ru>
2026-04-28 16:48:25 +0500

    - Починил тесты

--- tests/path_size_test.go
+++ tests/path_size_test.go
@@ -8,32 +8,32 @@ import (
 )
 
 func TestPath(t *testing.T) {
-	res := functions.GetPathSize("../testdata", false, false)
+	res := functions.GetPathSize("../testdata", false, false, false)
 	require.Equal(t, "32248B", res)
 
-	res = functions.GetPathSize("../testdata", false, true)
+	res = functions.GetPathSize("../testdata", false, true, false)
 	require.Equal(t, "39170B", res)
 
-	res = functions.GetPathSize("../testdata", true, false)
+	res = functions.GetPathSize("../testdata", true, false, false)
 	require.Equal(t, "31.5KB", res)
 
-	res = functions.GetPathSize("../testdata", true, true)
+	res = functions.GetPathSize("../testdata", true, true, false)
 	require.Equal(t, "38.3KB", res)
 
-	res = functions.GetPathSize("otherpath", true, true)
+	res = functions.GetPathSize("otherpath", true, true, false)
 	require.Equal(t, "Error: open otherpath: no such file or directory", res)
 
-	res = functions.GetPathSize("../testdata/emptydir", false, false)
+	res = functions.GetPathSize("../testdata/emptydir", false, false, false)
 	require.Equal(t, "0B", res)
 }
 
 func TestFile(t *testing.T) {
-	res := functions.GetPathSize("../testdata/text.txt", false, false)
+	res := functions.GetPathSize("../testdata/text.txt", false, false, false)
 	require.Equal(t, "25326B", res)
 
-	res = functions.GetPathSize("../testdata/text.txt", false, true)
+	res = functions.GetPathSize("../testdata/text.txt", false, true, false)
 	require.Equal(t, "0B", res)
 
-	res = functions.GetPathSize("../testdata/text.txt", true, false)
+	res = functions.GetPathSize("../testdata/text.txt", true, false, false)
 	require.Equal(t, "24.7KB", res)
 }
