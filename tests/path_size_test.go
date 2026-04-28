90130133ef9bb1b473ce040454052f4da3d5a81a
Aleksey Kononenko <a.knnnk@mail.ru>
2026-04-28 14:27:16 +0500

    - Добавил тест на исправленную ошибку

--- tests/path_size_test.go
+++ tests/path_size_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSimple(t *testing.T) {
+func TestPath(t *testing.T) {
 	res := functions.GetPathSize("../testdata")
 	require.Equal(t, "6936B", res)
 }
@@ -20,4 +20,9 @@ func TestEmptyPath(t *testing.T) {
 func TestUnexpectedPath(t *testing.T) {
 	res := functions.GetPathSize("otherpath")
 	require.Equal(t, "Error: open otherpath: no such file or directory", res)
+}
+
+func TestFile(t *testing.T) {
+	res := functions.GetPathSize("../testdata/test.txt")
+	require.Equal(t, "14B", res)
 }
